2a9c33c7689e2845e8a0554b8d9edc6fa94d57fb
doyi12 <36650439+doyi12@users.noreply.github.com>
2018-02-20 10:23:48 +0900

    Test1

--- src/com/yook/s1/Main.java
+++ src/com/yook/s1/Main.java
@@ -11,6 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 		SchoolController schoolcontroller = new SchoolController();
 		schoolcontroller.start();
+		System.out.println("program start");
 	}
 
 }
